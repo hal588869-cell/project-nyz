21d2f0a50e7e1c0380b9d7c22f750ef530bf4e15
hal588869-cell <hal588869@gmail.com>
2026-07-23 03:07:40 +0300

    App code-behind

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows;
+
+namespace project_nyz
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
